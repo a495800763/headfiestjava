d7fa315be6923c61c77dfcf140eeca56b6dd6a7f
liumq <495800763@qq.com>
2021-04-10 22:09:19 +0800

    继承4 面向对象下1

---
+++ src/com/liumq/simplejava/extendsheadfirst/InstanceInitTest.java
@@ -0,0 +1,12 @@
+package com.liumq.simplejava.extendsheadfirst;
+
+public class InstanceInitTest {
+    {
+        a=6;
+    }
+    int a = 9;
+
+    public static void main(String[] args) {
+        System.out.println(new InstanceInitTest().a);
+    }
+}
---
+++ src/com/liumq/simplejava/oopprehead/AutoBoxingUnboxing.java
@@ -0,0 +1,20 @@
+package com.liumq.simplejava.oopprehead;
+
+/**
+ * 包装类型的自动装箱拆箱
+ */
+public class AutoBoxingUnboxing {
+    public static void main(String[] args) {
+        //直接把一个基本类型变量赋值给Integer对象
+        Integer inObj= 5;
+        //直接把一个boolean类型的变量赋值给一个Object类型
+        Object boolObj = true;
+        //直接把一个Integer对象赋值给一个int 类型的变量
+        int it = inObj;
+        if(boolObj instanceof  Boolean)
+        {
+            boolean b = (Boolean) boolObj;
+            System.out.println(b);
+        }
+    }
+}
