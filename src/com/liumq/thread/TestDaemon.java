4846ea4fb0e71c738f992f35b96ddd324a8869df
liumq <495800763@qq.com>
2021-05-29 18:17:15 +0800

    java  狂神 多线程6

---
+++ src/com/liumq/thread/TestDaemon.java
@@ -0,0 +1,45 @@
+package com.liumq.thread;
+
+/**
+ * 测试守护线程
+ *
+ * 虚拟机不管守护线程是否会停止，只要用户线程停止了 守护线程也会停止
+ */
+public class TestDaemon {
+
+
+    public static void main(String[] args) {
+        God god = new God();
+        You you = new You();
+
+        Thread thread = new Thread(god);
+        thread.setDaemon(true);//默认false 是用户线程，一般的线程都是用户线程true 是守护线程
+        thread.start();
+
+        new Thread(you).start();//用户线程启动，用户线程会停止
+    }
+
+
+}
+
+//上帝
+class God implements Runnable {
+    @Override
+    public void run() {
+        while (true) {
+            System.out.println("上帝保佑着你");
+        }
+    }
+}
+
+//你
+class You implements Runnable {
+    @Override
+    public void run() {
+        for (int i = 0; i < 36500; i++) {
+            System.out.println("你每天都开心得活着!!!");
+        }
+
+        System.out.println("你离开了这个世界");
+    }
+}
---
+++ src/com/liumq/thread/TestJoin.java
@@ -0,0 +1,27 @@
+package com.liumq.thread;
+
+/**
+ * 测试join方法,插队
+ */
+public class TestJoin implements Runnable {
+    @Override
+    public void run() {
+        for (int i = 0; i < 1000; i++) {
+            System.out.println("VIP 来了" + i);
+        }
+
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        TestJoin join = new TestJoin();
+        Thread t = new Thread(join);
+        t.start();
+
+        for (int i = 0; i < 500; i++) {
+            if (i == 200) {
+                t.join();
+            }
+            System.out.println("main 在执行：" + i);
+        }
+    }
+}
